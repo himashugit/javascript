440f0ea1b57ab6755b633bbf9ac7ef109634d10e
Himanshu <Himanshu.cloudconsultant@gmail.com>
2021-06-28 21:56:52 -0400

    added second third html file

---
+++ my.js
@@ -0,0 +1,12 @@
+
+/*library = framework, where you put your code with the functions and create a programm*/
+
+function myfirst () {
+
+  alert('hello himanshu');
+
+}
+
+function myloc() {
+    document.location = 'http://www.google.com';
+}
